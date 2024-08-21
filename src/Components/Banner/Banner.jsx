aa66d644cf4c5ca25ee60a61a5fd50fe7d414fbd
Sadi Mridha <sadimridha6@gmail.com>
2024-08-22 00:17:59 +0600

    added hero section with responsive

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -2,8 +2,14 @@
 
 const Banner = () => {
     return (
-        <div>
-            This is banner
+        <div className="flex items-center justify-around py-10 bg-slate-100 rounded-3xl text-black">
+            <div className="pl-20">
+                <h1 className="text-6xl mb-6 font-bold">Books to freshen up your bookshelf</h1>
+                <button className='bg-green-500 px-4 py-2  text-white text-xl rounded-md'>View The List</button>
+            </div>
+            <div className="w-2/4">
+                <img src="https://i.ibb.co/J5gC2TM/pngwing-1.png" alt="banner book" />
+            </div>
         </div>
     );
 };
--- src/Components/Home/Home .jsx
+++ src/Components/Home/Home .jsx
@@ -1,8 +1,9 @@
+import Banner from "../Banner/Banner";
 
 const Home  = () => {
     return (
         <div>
-            
+            <div className="mt-20"><Banner /></div>
         </div>
     );
 };
